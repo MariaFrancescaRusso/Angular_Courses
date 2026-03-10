7ba144ff16a7bb1675f702f0160b914e5bb4b8a9
MariaFrancescaRusso <maria.russo10@studio.unibo.it>
2026-03-10 22:09:36 +0100

    add component sign-up-form

--- course-booking-system/src/app/app.routes.ts
+++ course-booking-system/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { CoursesList } from './courses-list/courses-list';
 import { CourseDetail } from './course-detail/course-detail';
 import { About } from './about/about';
+import { SignUpForm } from './sign-up-form/sign-up-form';
 
 export const routes: Routes = [
 	// Our two main routes:
 	{ path: '', redirectTo: 'courses', pathMatch: 'full' },
 	{ path: 'courses', component: CoursesList },
 	{ path: 'courses/:id', component: CourseDetail },
+	{ path: 'sign-up', component: SignUpForm },
 	{ path: 'about', component: About }
 ];
