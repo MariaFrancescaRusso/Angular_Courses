673da2b98fdfa7d9f1ab9154b6eda5fd17930446
MariaFrancescaRusso <maria.russo10@studio.unibo.it>
2026-03-12 20:21:03 +0100

    handle submit form

--- course-booking-system/src/app/services/course.service.ts
+++ course-booking-system/src/app/services/course.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Course } from '../models/course.model';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Student } from '../models/student.model';
 
 @Injectable({
   providedIn: 'root',
@@ -22,7 +23,7 @@ export class CourseService {
   constructor(private http: HttpClient) {}
   
   // GET All Courses or Courses filtred by optional description
-  getCourses(description?: string | null) : Observable<Course[]> {
+  getCourses(description?: string | null): Observable<Course[]> {
     let url = `${this.baseUrl}/courses`;
     if (description)
       url += `?description=${description}`;
@@ -30,12 +31,17 @@ export class CourseService {
   }
 
   // GET Course by ID
-  getCourseByID(id: number) : Observable<Course> {
+  getCourseByID(id: number): Observable<Course> {
     return this.http.get<Course>(`${this.baseUrl}/courses/${id}`);
   }
   
   // ADD New Course
-  addCourse(course: Course) : Observable<Course> {
+  addCourse(course: Course): Observable<Course> {
     return this.http.post<Course>(`${this.baseUrl}/courses`, course);
+  }
+
+  // ADD New Student to a Course
+  addStudent(student: Student): Observable<Student> {
+    return this.http.post<Student>(`${this.baseUrl}/students`, student);
   }
 }
--- course-booking-system/src/app/sign-up-form/sign-up-form.ts
+++ course-booking-system/src/app/sign-up-form/sign-up-form.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CourseService } from '../services/course.service';
 import { Course } from '../models/course.model';
+import { Student } from '../models/student.model';
 
 @Component({
   selector: 'app-sign-up-form',
@@ -13,6 +14,8 @@ import { Course } from '../models/course.model';
 export class SignUpForm implements OnInit {
   signUpForm!: FormGroup;
   courses: Course[] = [];
+  submissionSuccess: boolean = false;
+  submissionError: string = "";
 
   constructor(private formBuilder: FormBuilder, private courseService: CourseService) {}
 
@@ -44,5 +47,32 @@ export class SignUpForm implements OnInit {
 
   get enrolledCourseId() {
     return this.signUpForm.get('enrolledCourseId');
+  }
+
+  onSubmit(): void {
+    if(this.signUpForm.invalid)
+      return;
+
+    const newStudent: Student = {
+      id: 0,
+      name: this.signUpForm.value.name,
+      email: this.signUpForm.value.email,
+      enrolledCourseIds: [this.signUpForm.value.enrolledCourseId],
+      // phoneNumber: this.signUpForm.value.phoneNumber,
+      // registrationDate: this.signUpForm.value.registrationDate
+    };
+
+    this.courseService.addStudent(newStudent).subscribe({
+      next: (student) => {
+        console.log("Student sussessfully signed up: ", student);
+        this.submissionSuccess = true;
+        this.signUpForm.reset();
+      },
+
+      error: (err) => {
+        console.error('Error signing up student: ', err);
+        this.submissionError = "There was an error submitting your sign-up. Please try again.";
+      }
+    });
   }
 }
