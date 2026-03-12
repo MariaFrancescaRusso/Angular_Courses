4c301ad60a174f5b43007bd7b7b1d899cd69cd8e
MariaFrancescaRusso <maria.russo10@studio.unibo.it>
2026-03-12 16:53:41 +0100

    add validation to sign-up-form

--- course-booking-system/src/app/sign-up-form/sign-up-form.ts
+++ course-booking-system/src/app/sign-up-form/sign-up-form.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CourseService } from '../services/course.service';
 import { Course } from '../models/course.model';
 
@@ -18,9 +18,9 @@ export class SignUpForm implements OnInit {
 
   ngOnInit(): void {
     this.signUpForm = this.formBuilder.group({
-      name: [''],
-      email: [''],
-      enrolledCourseId: [null]
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      enrolledCourseId: [null, Validators.required]
     });
 
     this.courseService.getCourses().subscribe({
