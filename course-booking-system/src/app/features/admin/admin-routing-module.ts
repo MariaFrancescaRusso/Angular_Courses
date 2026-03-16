06b7de041122698d831223aab1ed7de494bbac38
MariaFrancescaRusso <maria.russo10@studio.unibo.it>
2026-03-16 19:50:42 +0100

    add dashboard component to handle admin module

--- course-booking-system/src/app/features/admin/admin-routing-module.ts
+++ course-booking-system/src/app/features/admin/admin-routing-module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddNewCourseForm } from './add-new-course-form/add-new-course-form';
-import { App } from '../../app';
 import { StudentList } from './student-list/student-list';
+import { Dashboard } from './dashboard/dashboard';
 
 const routes: Routes = [
   {
     path: '',
-    component: App,
+    component: Dashboard,
     children: [
+      { path: '', redirectTo: 'students', pathMatch: 'full' },
       { path: 'add-new-course', component: AddNewCourseForm },
       { path: 'students', component: StudentList }
     ]
